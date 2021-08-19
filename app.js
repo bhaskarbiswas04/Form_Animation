24445b9bb4b587e11c859225c1991adee47076c5
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2021-08-19 22:36:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function animatedForm()
             parent.addEventListener('animationend', () => {
                 parent.style.animation = "";
             });
-        } );
+        });
     });
 }
 
